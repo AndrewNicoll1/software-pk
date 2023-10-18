c35ef7c00e4e262f2117bed16419fdb704b44f54
Edward Blackman <ehblackman500@gmail.com>
2023-10-18 12:17:50 +0100

    added a test for the dose rate function but it does NOT WORK YET. Need to decide parameters

--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -1,5 +1,7 @@
 import unittest
-import pkmodel as pk
+import pytest
+import pkmodel
+from pkmodel.model import Model
 
 
 class ModelTest(unittest.TestCase):
@@ -10,6 +12,26 @@ class ModelTest(unittest.TestCase):
         """
         Tests Model creation.
         """
-        model = pk.Model()
+        model = pkmodel()
         self.assertEqual(model.value, 42)
 
+
+
+class TestDoseFunction(unittest.TestCase):
+    
+    def test_valid_number_input(self):
+        # Test with a valid numeric input
+        model_args = {'X': 5.0}  # Replace with your actual model parameters
+        model = Model(model_args=model_args)  
+        result = model.dose(1.0)
+        self.assertIsInstance(result, (int, float), "Input must be a number (int or float)")
+    
+    def test_invalid_string_input(self):
+        # Test with an invalid string input
+        model_args = {'X': "invalid"}  # Replace with your actual model parameters
+        model = Model(model_args=model_args)  
+        with self.assertRaises(AssertionError):
+            result = model.dose(1.0)
+
+if __name__ == '__main__':
+    unittest.main()
